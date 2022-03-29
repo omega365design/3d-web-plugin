d9d7d55ccd189ecd8d3bf38c2b49a218e995baf9
Espen Gjendem <espen.gjendem@gmail.com>
2022-03-29 09:57:36 +0200

    changed to directional light

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import React, { useRef, Suspense, useEffect } from "react";
-import { Canvas, useFrame, useLoader, useThree } from "@react-three/fiber";
+import { Canvas, useFrame, useLoader, useThree, directionalLight } from "@react-three/fiber";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import CircularProgress from "@mui/material/CircularProgress";
@@ -14,6 +14,8 @@ function CameraController() {
     controls.minDistance = 1;
     controls.maxDistance = 20000;
 
+    // camera.position
+
     return () => {
       controls.dispose();
     };
@@ -25,12 +27,15 @@ function CameraController() {
 function RenderComponent() {
   const ref = useRef();
   const gltf = useLoader(GLTFLoader, window.modelUrl);
+
   // useFrame((state, delta) => (ref.current.rotation.x += 0.01));
   return (
     <Canvas>
       <CameraController />
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} castShadow />
+      <ambientLight intensity={0.4} />
+      <directionalLight intensity={0.5} position={[1, 0.5, 0.75]} castShadow />
+      <directionalLight intensity={0.5} position={[-1, -0.5, -0.75]} castShadow />
+      {/* <directionalLight position={[-10, -20, -30]} castShadow /> */}
       <primitive object={gltf.scene} position={[0, 0, 0]} scale={0.0005} />
     </Canvas>
   );
@@ -43,6 +48,7 @@ function CircularIndeterminate() {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
+        height: "400px"
       }}
     >
       <CircularProgress style={{ color: "#020d2e" }} />
